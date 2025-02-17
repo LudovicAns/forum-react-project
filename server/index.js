7349425a6a3d8034ace6afbc6bf762e2d9c74d65
LudovicAns <ludovicanselin@gmail.com>
2025-02-17 11:21:27 +0100

    feat: load port from environment variable
    Replaced hardcoded port with environment variable `APP_PORT` from `.env` for improved configurability. Added dotenv package for environment variable support and adjusted `.gitignore` to correctly handle `.env` files in the server directory.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import 'dotenv/config'
 
 const app = express();
 // todo: use env variable
-const port = 3000;
+const PORT = process.env.APP_PORT;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+if (!PORT) {
+    throw new Error('PORT is not defined');
+}
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
