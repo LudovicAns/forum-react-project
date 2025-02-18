9839b75e94bb5fece3d9ac2d0f89bae91d8bc71b
LudovicAns <ludovicanselin@gmail.com>
2025-02-18 09:20:54 +0100

    feat: enhance Link component with href fallback
    Added a fallback for the `href` property in the `Link` component to ensure it derives from `to` if `href` is not provided. This improves consistency and avoids potential issues when `to` is used without an explicit `href`.

--- client/src/component/catalyst-ui/link.jsx
+++ client/src/component/catalyst-ui/link.jsx
@@ -6,13 +6,18 @@
  */
 
 import * as Headless from '@headlessui/react'
-import React, { forwardRef } from 'react'
-import { Link as ReactRouterLink } from "react-router";
+import React, {forwardRef} from 'react'
+import {Link as ReactRouterLink} from "react-router";
 
 export const Link = forwardRef(function Link(props, ref) {
-  return (
-    <Headless.DataInteractive>
-      <ReactRouterLink {...props} ref={ref} />
-    </Headless.DataInteractive>
-  )
+
+    if (!props.href && props.to) {
+        props.href = props.to;
+    }
+
+    return (
+        <Headless.DataInteractive>
+            <ReactRouterLink {...props} to={props.href} ref={ref}/>
+        </Headless.DataInteractive>
+    )
 })
