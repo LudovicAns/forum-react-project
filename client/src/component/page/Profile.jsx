d6100494054d3a1d09e38f499d52a7f4a877578d
LudovicAns <ludovicanselin@gmail.com>
2025-02-18 18:03:24 +0100

    feat: enhance Profile page UI with Subheading and Divider
    Added Subheading to improve hierarchy and visual clarity in the "Modification" section. Included a Divider component to better separate content sections. These changes aim to enhance the user experience and readability.

--- client/src/component/page/Profile.jsx
+++ client/src/component/page/Profile.jsx
@@ -1,10 +1,11 @@
 import React, {useContext} from 'react';
-import {Heading} from "../catalyst-ui/heading.jsx";
+import {Heading, Subheading} from "../catalyst-ui/heading.jsx";
 import {Strong, Text} from "../catalyst-ui/text.jsx";
 import {UserContext} from "../../context/UserContextProvider.jsx";
 import ProfilWidget from "../widget/ProfilWidget.jsx";
 import {Button} from "../catalyst-ui/button.jsx";
 import {Badge} from "../catalyst-ui/badge.jsx";
+import {Divider} from "../catalyst-ui/divider.jsx";
 
 function Profile() {
 
@@ -17,12 +18,12 @@ function Profile() {
                 <Text>Votre page de profil vous permet de consulter les informations visible par les autres
                     utilisateurs.</Text>
                 <ProfilWidget className={"mt-4"}/>
-            </section>
-            <section>
-                <Heading>Modification</Heading>
+
+                <Subheading className={"mt-4"}>Modification</Subheading>
                 <Text className={"mb-4"}>Vous pouvez modifier votre profil en cliquant sur le bouton ci-dessous.</Text>
                 <Button href={"/profile/edit"}>Modifier</Button>
             </section>
+            <Divider/>
             <section>
                 <Heading>
                     Statistiques <Badge color={"yellow"}>Bientôt disponible</Badge>
