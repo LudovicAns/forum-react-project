23698085570c3f19677ee5a5fb9fa8588159a62f
LudovicAns <ludovicanselin@gmail.com>
2025-02-19 09:09:13 +0100

    feat: enhance ProfilWidget with description section and layout
    Added a description section to display user descriptions or a fallback text if unavailable. Improved the layout for better structure using Subheading, Divider, and Text components. Prepared the widget for a more detailed user interface experience.

--- client/src/component/widget/ProfilWidget.jsx
+++ client/src/component/widget/ProfilWidget.jsx
@@ -5,33 +5,49 @@ import {DescriptionDetails, DescriptionList, DescriptionTerm} from "../catalyst-
 import {Badge} from "../catalyst-ui/badge.jsx";
 import {UserContext} from "../../context/UserContextProvider.jsx";
 import clsx from "clsx";
+import {Subheading} from "../catalyst-ui/heading.jsx";
+import {Divider} from "../catalyst-ui/divider.jsx";
+import {Text} from "../catalyst-ui/text.jsx";
 
 function ProfilWidget({className}) {
     const userContext = useContext(UserContext);
 
     return (
-        <div
-            className={clsx(className, "flex flex-row max-md:flex-col gap-16 border p-8 rounded-lg border-zinc-950/10 dark:border-white/10")}>
-            <div className={"w-fit max-md:w-full flex justify-center"}>
-                <Avatar src={avatar} alt={"avatar"} className={"size-48"}/>
-            </div>
-            <div className={"w-full"}>
-                <DescriptionList className={"w-full"}>
-                    <DescriptionTerm>Nom d'utilisateur</DescriptionTerm>
-                    <DescriptionDetails>{userContext.user.username}</DescriptionDetails>
-
-                    <DescriptionTerm>Email</DescriptionTerm>
-                    <DescriptionDetails>{userContext.user.email}</DescriptionDetails>
-
-                    <DescriptionTerm>Rôle</DescriptionTerm>
-                    <DescriptionDetails>
-                        <Badge
-                            color={"yellow"}>{userContext.user.role ? userContext.user.role : "Bientôt disponible"}</Badge>
-                    </DescriptionDetails>
-                </DescriptionList>
-                <div className={"h-full"}>
-
+        <div className={clsx(className, "flex flex-col gap-8 border p-8 rounded-lg border-zinc-950/10 dark:border-white/10")}>
+            <div
+                className={clsx(className, "flex flex-row max-md:flex-col gap-16")}>
+                <div className={"w-fit max-md:w-full flex justify-center"}>
+                    <Avatar src={avatar} alt={"avatar"} className={"size-48"}/>
                 </div>
+                <div className={"w-full"}>
+                    <DescriptionList className={"w-full"}>
+                        <DescriptionTerm>Nom d'utilisateur</DescriptionTerm>
+                        <DescriptionDetails>{userContext.user.username}</DescriptionDetails>
+
+                        <DescriptionTerm>Email</DescriptionTerm>
+                        <DescriptionDetails>{userContext.user.email}</DescriptionDetails>
+
+                        <DescriptionTerm>Rôle</DescriptionTerm>
+                        <DescriptionDetails>
+                            <Badge
+                                color={"yellow"}>{userContext.user.role ? userContext.user.role : "Bientôt disponible"}</Badge>
+                        </DescriptionDetails>
+                    </DescriptionList>
+                    <div className={"h-full"}>
+
+                    </div>
+                </div>
+            </div>
+            <Divider/>
+            <div>
+                <Subheading>Description</Subheading>
+                {
+                    !userContext.user.description || userContext.user.description === "" ?
+                        <Text>Aucune description</Text>
+                        :
+                        <Text>{userContext.user.description}</Text>
+                }
+                <Text>{userContext.user.description}</Text>
             </div>
         </div>
     );
