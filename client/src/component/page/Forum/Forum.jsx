36f37b899c94365bcb3fa1692801fbeb3fdb9c9b
LudovicAns <ludovicanselin@gmail.com>
2025-02-20 16:36:52 +0100

    feat: integrate dynamic post fetching in forum page
    Replaced hardcoded post data with dynamic fetching from the backend using Axios. Added user-specific badge and improved new post handling, including a call-to-action when no posts are available. This enhances user experience and ensures the forum data is always up-to-date.

--- client/src/component/page/Forum/Forum.jsx
+++ client/src/component/page/Forum/Forum.jsx
@@ -1,89 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PostCardList from "../../widget/forum/PostCardList.jsx";
 import PostCard from "../../widget/forum/PostCard.jsx";
 import {Button} from "../../catalyst-ui/button.jsx";
 import {ArchiveBoxIcon, ArrowPathIcon, PlusCircleIcon, StarIcon} from "@heroicons/react/20/solid/index.js";
-import {Subheading} from "../../catalyst-ui/heading.jsx";
+import {Heading, Subheading} from "../../catalyst-ui/heading.jsx";
 import {Switch, SwitchField, SwitchGroup} from "../../catalyst-ui/switch.jsx";
 import {Fieldset, Label, Legend} from "../../catalyst-ui/fieldset.jsx";
 import {Text, TextLink} from "../../catalyst-ui/text.jsx";
 import {Divider} from "../../catalyst-ui/divider.jsx";
 import {Badge} from "../../catalyst-ui/badge.jsx";
+import axios from "axios";
 
-const posts = [
-    {
-        _id: "1",
-        title: "🚀 Voici le premier post !",
-        content: "azerfazerc vajkzefaiu zerhfiuahiuzehnczfhiz fuhiuazhf iuazfhauizfh aiuzhfiuazhfioauzhfiuazhfiuazhfiuazf" +
-            "auzehfiuaozhfgoiazfo iazhjfoiazhfiozahf iozaufhiuzahfiouz afhiu zhfiuoazf hizuahfiouzfhiaozfhioazfhi" +
-            "azjfbazljf laizufak zlfbhlkz jafhbi kzufhbafh biazlufbhiazulfg hiuazfghiu azfhgau ziofhioazufgh ziauofg huiazof" +
-            "azoi uefhao ziufhoifha zoiupf haoz piufhdscn oakzjnapo ziuefhaz poiufzjcnpozfhdopaziuefdhpzaofhaozmiiho",
-        author: "67b5e3405b26c118be41caf9",
-        comments: [
-            {
-                author: "67b5e3405b26c118be41caf9",
-                content: "Comment 1"
-            },
-            {
-                author: "67b5e3405b26c118be41caf8",
-                content: "Comment 2"
-            },
-        ]
-    },
-    {
-        _id: "2",
-        title: "Post 2",
-        content: "Content",
-        author: "67b5e3405b26c118be41caf9",
-        comments: [
-            {
-                author: "67b5e3405b26c118be41caf9",
-                content: "Comment 1"
-            },
-            {
-                author: "67b5e3405b26c118be41caf8",
-                content: "Comment 2"
-            },
-        ]
-    },
-    {
-        _id: "3",
-        title: "Post 3",
-        content: "Content",
-        author: "67b5e3405b26c118be41caf9",
-        comments: [
-            {
-                author: "67b5e3405b26c118be41caf9",
-                content: "Comment 1"
-            },
-            {
-                author: "67b5e3405b26c118be41caf8",
-                content: "Comment 2"
-            },
-        ]
-    },
-    {
-        _id: "4",
-        title: "Post 4",
-        content: "Content",
-        author: "67b5e3405b26c118be41caf9",
-        comments: [
-            {
-                author: "67b5e3405b26c118be41caf9",
-                content: "Comment 1"
-            },
-            {
-                author: "67b5e3405b26c118be41caf8",
-                content: "Comment 2"
-            },
-        ]
-    }
-]
+const availableSoon = (<Badge color={"yellow"}>Bientôt disponible</Badge>);
 
-const availableSoon = <Badge color={"yellow"}>Bientôt disponible</Badge>;
+const newPostButton = (
+    <Button href={"/forum/new-post"} color={"blue"} className={"w-full"}>
+        <PlusCircleIcon className={"fill-white dark:fill-white"}/>
+        Ajouter un post
+    </Button>
+);
 
 function Forum(props) {
     document.title = "Forum - Forum";
+
+    const [posts, setPosts] = React.useState([]);
+
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_BACKEND_HOST}api/posts`, {withCredentials: true})
+            .then(res => {
+                if (res.status === 204) return [];
+                console.log(res.data.data);
+                setPosts(res.data.data);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }, []);
 
     return (
         <main className={"flex flex-row max-lg:flex-col gap-4"}>
@@ -100,10 +52,7 @@ function Forum(props) {
                         Vos favoris
                     </TextLink>
                     <Divider className={"my-4"}/>
-                    <Button href={"/forum/new-post"} color={"blue"} className={"w-full"}>
-                        <PlusCircleIcon className={"fill-white dark:fill-white"}/>
-                        Ajouter un post
-                    </Button>
+                    {newPostButton}
                 </div>
                 <div>
                     <form>
@@ -131,19 +80,28 @@ function Forum(props) {
                     </form>
                 </div>
             </div>
-            <PostCardList>
-                {
-                    posts &&
-                    posts.map(post => {
-                        return (
-                            <PostCard key={post._id} post={post}/>
-                        );
-                    })
-                }
-            </PostCardList>
+            {
+                posts.length > 0 ?
+                    <PostCardList>
+                        {
+                            posts &&
+                            posts.map(post => {
+                                return (
+                                    <PostCard key={post._id} post={post}/>
+                                );
+                            })
+                        }
+                    </PostCardList>
+                    :
+                    <div className={"flex flex-col gap-4 w-full justify-center items-center"}>
+                        <Heading>Aucun post, soit le premier à poster !</Heading>
+                        <div>
+                            {newPostButton}
+                        </div>
+                    </div>
+            }
         </main>
-    )
-        ;
+    );
 }
 
 export default Forum;
--- client/src/component/widget/forum/PostCard.jsx
+++ client/src/component/widget/forum/PostCard.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Subheading} from "../../catalyst-ui/heading.jsx";
 import {Text, TextLink} from "../../catalyst-ui/text.jsx";
 import {Divider} from "../../catalyst-ui/divider.jsx";
 import {Button} from "../../catalyst-ui/button.jsx";
 import {Avatar} from "../../catalyst-ui/avatar.jsx";
 import {BookOpenIcon, ChatBubbleOvalLeftEllipsisIcon} from "@heroicons/react/20/solid/index.js";
+import {UserContext} from "../../../context/UserContextProvider.jsx";
+import {Badge} from "../../catalyst-ui/badge.jsx";
 
 function PostCard({post}) {
+
+    const userContext = useContext(UserContext);
 
     return (
         <div className={"flex flex-col gap-2 w-full h-fit border border-zinc-950/10 dark:border-white/10 rounded-md p-8"}>
             <div className={"flex flex-row gap-2 justify-between"}>
-                <Subheading>{post.title}</Subheading>
-                <TextLink target={"_blank"} href={`/profile/${post.author}`} className={"flex flex-row gap-2 !no-underline"}>
-                    <Avatar className={"size-[24px] dark:bg-white dark:text-black bg-black text-white"} src={""} initials={"A"}/>
-                    <Text className={"max-md:hidden text-zinc-950 dark:!text-white"}>AnonymeUser</Text>
+                <Subheading>
+                    {post.title} {post.author.id === userContext.user.id && <Badge color={"red"}>Propriétaire</Badge>}
+                </Subheading>
+                <TextLink target={"_blank"} href={`/profile/${post.author.id}`} className={"flex flex-row gap-2 !no-underline"}>
+                    <Avatar className={"size-[24px] dark:bg-white dark:text-black bg-black text-white"} src={`${import.meta.env.VITE_BACKEND_HOST}${post.author.avatar}`} initials={"A"}/>
+                    <Text className={"max-md:hidden text-zinc-950 dark:!text-white"}>{post.author.username}</Text>
                 </TextLink>
             </div>
             <div className={"h-12 max-h-12"}>
