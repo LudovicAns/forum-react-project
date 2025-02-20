0cd4368667d36a14ce51a32c73fdbc73728827b9
LudovicAns <ludovicanselin@gmail.com>
2025-02-20 09:23:39 +0100

    fix: correct PostCard author link structure and styling
    Updated PostCard to ensure the author link opens in a new tab and fixed alignment and visibility issues for the author's name and avatar. This improves both the user experience and consistency across components.

--- client/src/component/widget/forum/PostCard.jsx
+++ client/src/component/widget/forum/PostCard.jsx
@@ -12,10 +12,10 @@ function PostCard({post}) {
         <div className={"flex flex-col gap-2 w-full h-fit border border-zinc-950/10 dark:border-white/10 rounded-md p-8"}>
             <div className={"flex flex-row gap-2 justify-between"}>
                 <Subheading>{post.title}</Subheading>
-                <div className={"flex flex-row gap-2"}>
+                <TextLink target={"_blank"} href={`/profile/${post.author}`} className={"flex flex-row gap-2 !no-underline"}>
                     <Avatar className={"size-[24px] dark:bg-white dark:text-black bg-black text-white"} src={""} initials={"A"}/>
-                    <TextLink href={`/profile/${post.author}`} className={"!no-underline max-md:hidden"}>AnonymeUser</TextLink>
-                </div>
+                    <Text className={"max-md:hidden text-zinc-950 dark:!text-white"}>AnonymeUser</Text>
+                </TextLink>
             </div>
             <div className={"h-12 max-h-12"}>
                 <Text className={"h-full overflow-hidden"}>{post.content}</Text>
@@ -23,7 +23,7 @@ function PostCard({post}) {
             <Divider className={"my-2"}/>
             <div className={"flex-row flex gap-1"}>
                 <ChatBubbleOvalLeftEllipsisIcon className={"size-[22px] fill-zinc-500"}/>
-                <Text className={""}>
+                <Text>
                     {post.comments.length}
                 </Text>
             </div>
