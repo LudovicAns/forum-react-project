390250ce79f3485ed29d184661d9917463dfdf1d
LudovicAns <ludovicanselin@gmail.com>
2025-02-21 10:47:18 +0100

    feat: add owner actions (edit and delete) to forum posts
    Introduce buttons for post owners to edit or delete their posts. The edit action redirects to the edit post page, while the delete action includes a placeholder for future implementation. Display these options only to the post owner for enhanced user experience.

--- client/src/component/page/Forum/PublicPost.jsx
+++ client/src/component/page/Forum/PublicPost.jsx
@@ -8,6 +8,8 @@ import {Heading} from "../../catalyst-ui/heading.jsx";
 import UserAvatarWidget from "../../widget/UserAvatarWidget.jsx";
 import WritePostCommentWidget from "../../widget/forum/WritePostCommentWidget.jsx";
 import CommentCard from "../../widget/forum/CommentCard.jsx";
+import {Button} from "../../catalyst-ui/button.jsx";
+import {PencilSquareIcon, TrashIcon} from "@heroicons/react/20/solid/index.js";
 
 function PublicPost(props) {
 
@@ -39,27 +41,52 @@ function PublicPost(props) {
             });
     }, []);
 
+    const isOwner = userContext.user.id === post?.author.id;
+
+    const ownerActions = (
+        <div className={"flex flex-row gap-4 w-full justify-end max-lg:justify-center mb-4"}>
+            <Button color={"light"} className={"cursor-pointer !text-blue-500"} href={"/forum/edit-post/" + id}>
+                <PencilSquareIcon className={"fill-blue-500"}/>
+                Modifier
+            </Button>
+            <Button color={"light"} className={"cursor-pointer !text-red-500"} onClick={() => {console.log("todo: supprimer")}}>
+                <TrashIcon className={"fill-red-500"}/>
+                Supprimer
+            </Button>
+        </div>
+    );
+
     return (
         <main>
             {
                 !loading &&
                 <>
                     <div>
-                        <div className={"flex flex-row justify-between"}>
-                            <Heading>{post.title}</Heading>
-                            <UserAvatarWidget user={post.author}/>
-                        </div>
                         {
-                            post.content.split("\n").map((line, i) => (
-                                <Text key={i}>
-                                    {line}
-                                </Text>
-                            ))
+                            isOwner && (
+                                <>
+                                    {ownerActions}
+                                </>
+                            )
                         }
+                        <div className={"border border-zinc-950/10 dark:border-white/10 rounded-md p-8"}>
+                            <div className={"flex flex-row justify-between mb-4"}>
+                                <Heading>{post.title}</Heading>
+                                <UserAvatarWidget user={post.author}/>
+                            </div>
+                            <Divider className={"my-4"}/>
+                            {
+                                post.content.split("\n").map((line, i) => (
+                                    <Text key={i}>
+                                        {line}
+                                    </Text>
+                                ))
+                            }
+                        </div>
                     </div>
                     <Divider className={"my-4"}/>
                     {
-                        userContext.user.id !== post.author.id && (
+                        !isOwner && (
                             <WritePostCommentWidget post={post}/>
                         )
                     }
