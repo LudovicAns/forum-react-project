1229816341ed3c94f3049816231a79389affbe93
LudovicAns <ludovicanselin@gmail.com>
2025-02-22 12:39:13 +0100

    feat: add population for comments and implement comment creation
    Added population for 'author' and 'post' fields in CommentRepository with transformation logic using userDto. Updated all repository methods to include populated data. Also, improved the comment creation endpoint in the controller by integrating CommentService.

--- server/controller/comment-controller.js
+++ server/controller/comment-controller.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import AuthMiddleware from "../middleware/auth-middleware.js";
+import {CommentService} from "../service/comment-service.js";
 
 const router = express.Router();
 
 router.post("/", AuthMiddleware, async (req, res) => {
     // Implement code.
+    const newComment = req.body;
+
+    CommentService.addComment(newComment)
+        .then(comment => {
+            return res.status(201).json({
+                message: "Commentaire ajouté avec succès",
+                data: comment
+            })
+        })
+
     res.status(501).json({
         message: "Not implemented yet."
     });
--- server/model/dao/repository/comment-repository.js
+++ server/model/dao/repository/comment-repository.js
@@ -1,4 +1,23 @@
 import {Comment} from "../entity/comment.js";
+import {userDto} from "../../dto/user-dto.js";
+
+const populateComment = [
+    {
+        path: 'author',
+        transform: (doc, id) => {
+            return userDto(doc);
+        }
+    },
+    {
+        path: 'post',
+        transform: (doc, id) => {
+            return {
+                ...doc,
+                author: userDto(doc.author)
+            }
+        }
+    }
+]
 
 export const CommentRepository = {
     create: async function (comment) {
@@ -8,21 +27,21 @@ export const CommentRepository = {
 
     getById: async function (id) {
         try {
-            return await Comment.findOne({_id: id});
+            return await Comment.findOne({_id: id}).populate(populateComment).exec();
         } catch (error) {
             return null;
         }
     },
 
     getAll: async function () {
-        return await Comment.find();
+        return await Comment.find().populate(populateComment).exec();
     },
 
     updateById: async function (id, newComment) {
-        return await Comment.findByIdAndUpdate(id, newComment);
+        return await Comment.findByIdAndUpdate(id, newComment).populate(populateComment).exec();
     },
 
     deleteById: async function (id) {
-        return await Comment.findByIdAndDelete(id);
+        return await Comment.findByIdAndDelete(id).populate(populateComment).exec();
     }
 };
